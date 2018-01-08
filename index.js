a7e1b8bfda5fd0c907aee0ca1c07d6edfdec9a95
tentaculo-worker <hellotentaculo@gmail.com>
2018-01-08 10:56:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// ads
-
 var conversionType = "L2S";
 var resultValue = "---";
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
